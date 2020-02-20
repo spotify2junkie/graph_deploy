701ff7def6fefe9cb24c26040784b1d9f70329bc
spotify2junkie <lsemarkluo@gmail.com>
2020-02-20 15:44:28 +0800

    GAT base layer

---
+++ layer_utils/gat_layer.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GATlayer(nn.Module):
+    def __init__(self, g, in_dim, out_dim):
+        super(GATlayer, self).__init__()
+        self.g = g
+        self.fc = nn.Linear(in_dim, out_dim, bias=False)
+        self.attn_fc = nn.Linear(
+            2*out_dim, 1, bias=False)  # for attention calc
+
+    def message_func(self, edges):
+        # point out node-to-node edges
+        return {'z': edges.src['z'], 'e': edges.data['e']}
+
+    def attn_func(self, edges):
+        # applied to edges
+        z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
+        a = self.attn_fc(z2)
+        return {'e': F.leaky_relu(a)}
+
+    def reduce_func(self, nodes):
+        # reduce on nodes
+        alpha = F.softmax(nodes.mailbox['e'], dim=1)
+        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
+        return {'h': h}
+
+    def forward(self, h):
+        z = self.fc(h)
+        self.g.ndata['z'] = z
+        self.g.apply_edges(self.attn_func)
+        self.g.update_all(self.message_func, self.reduce_func)
+        return self.g.pop['h']
